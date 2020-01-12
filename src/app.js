269442ebe4036b0940ccd0891328d98e5fce87f4
Przemyslaw Koziel <mos.koziel@gmail.com>
2020-01-12 15:16:19 +0100

    App setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/styles.css";
+
+function App() {
+  const header = {
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    marginTop: "300px",
+    fontSize: "24px"
+  };
+  return <div style={header}>First commit</div>;
+}
+ReactDOM.render(<App />, document.getElementById("root"));
